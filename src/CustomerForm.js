34a0c5a4adc97002a3da1dfe11afe15b35569eaf
mar1n <30738216+mar1n@users.noreply.github.com>
2020-03-30 15:53:16 +0100

    test basic html form input

---
+++ src/CustomerForm.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const CustomerForm = () => (
+        <form id="customer">
+            <input
+                type="text"
+                name="firstName"
+            />
+        </form>
+    );
