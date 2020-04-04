5b82bfac39839490519a704d2684d9ce5a73ead8
mar1n <30738216+mar1n@users.noreply.github.com>
2020-04-05 00:33:38 +0100

    days and time list

--- src/AppointmentForm.js
+++ src/AppointmentForm.js
@@ -1,45 +1,107 @@
 import React, { useState } from 'react';
 
-const TimeSlotTable = () => <table id="time-slots" />;
+const timeIncrements = (numTimes, startTime, increment) =>
+    Array(numTimes)
+        .fill([startTime])
+        .reduce((acc, _, i) =>
+            acc.concat([startTime + i * increment])
+        );
+
+const dailyTimeSlots = (salonOpensAt, salonClosesAt) => {
+    const totalSlots = (salonClosesAt - salonOpensAt) * 2;
+    const startTime = new Date().setHours(salonOpensAt, 0, 0, 0);
+    const increment = 30 * 60 * 1000;
+    return timeIncrements(totalSlots, startTime, increment);
+};
+
+const weeklyDateValues = startDate => {
+    const midnight = new Date(startDate).setHours(0, 0, 0, 0);
+    const increment = 24 * 60 * 60 * 1000;
+    return timeIncrements(7, midnight, increment);
+};
+
+const toShortDate = timestamp => {
+    const [day, , dayOfMonth] = new Date(timestamp)
+        .toDateString()
+        .split(' ');
+    return `${day} ${dayOfMonth}`;
+};
+
+const toTimeValue = timestamp =>
+    new Date(timestamp).toTimeString().substring(0, 5);
+
+const TimeSlotTable = ({ salonOpensAt, salonClosesAt, today }) => {
+    const dates = weeklyDateValues(today);
+    const timeSlots = dailyTimeSlots(salonOpensAt, salonClosesAt);
+    return (
+        <table id="time-slots">
+            <thead>
+                <tr>
+                    <th />
+                    {dates.map(d => (
+                        <th key={d}>{toShortDate(d)}</th>
+                    ))}
+                </tr>
+            </thead>
+            <tbody>
+                {timeSlots.map(timeSlot => (
+                    <tr key={timeSlot}>
+                        <th>{toTimeValue(timeSlot)}</th>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+    );
+};
 
 export const AppointmentForm = ({
-  selectableServices,
-  service,
-  onSubmit
+    selectableServices,
+    service,
+    onSubmit,
+    salonOpensAt,
+    salonClosesAt,
+    today
 }) => {
-  const [appointment, setAppointment] = useState({ service });
+    const [appointment, setAppointment] = useState({ service });
 
-  const handleServiceChange = ({ target: { value } }) =>
-    setAppointment(appointment => ({
-      ...appointment,
-      service: value
-    }));
+    const handleServiceChange = ({ target: { value } }) =>
+        setAppointment(appointment => ({
+            ...appointment,
+            service: value
+        }));
 
-  return (
-    <form id="appointment" onSubmit={() => onSubmit(appointment)}>
-      <label htmlFor="service">Salon service</label>
-      <select
-        name="service"
-        id="service"
-        value={service}
-        onChange={handleServiceChange}>
-        <option />
-        {selectableServices.map(s => (
-          <option key={s}>{s}</option>
-        ))}
-      </select>
-      <TimeSlotTable />
-    </form>
-  );
+    return (
+        <form id="appointment" onSubmit={() => onSubmit(appointment)}>
+            <label htmlFor="service">Salon service</label>
+            <select
+                name="service"
+                id="service"
+                value={service}
+                onChange={handleServiceChange}>
+                <option />
+                {selectableServices.map(s => (
+                    <option key={s}>{s}</option>
+                ))}
+            </select>
+            <TimeSlotTable
+                salonOpensAt={salonOpensAt}
+                salonClosesAt={salonClosesAt}
+                today={today}
+            />
+        </form>
+    );
 };
 
 AppointmentForm.defaultProps = {
-  selectableServices: [
-    'Cut',
-    'Blow-dry',
-    'Cut & color',
-    'Beard trim',
-    'Cut & beard trim',
-    'Extensions'
-  ]
+    today: new Date(),
+    salonOpensAt: 9,
+    salonClosesAt: 19,
+    selectableServices: [
+        'Cut',
+        'Blow-dry',
+        'Cut & color',
+        'Beard trim',
+        'Cut & beard trim',
+        'Extensions'
+    ]
 };
