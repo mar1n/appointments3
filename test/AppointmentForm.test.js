83af3db49fb51fa753cb3794f76494ce576c1b03
mar1n <30738216+mar1n@users.noreply.github.com>
2020-04-05 11:25:01 +0100

    hide radio buttons trest

--- test/AppointmentForm.test.js
+++ test/AppointmentForm.test.js
@@ -99,7 +99,14 @@ describe('AppointmentForm', () => {
           });
     });
     const timeSlotTable = () => container.querySelector('table#time-slots');
+    const startsAtField = index =>
+        container.querySelectorAll(`input[name="startsAt"]`)[index];
     describe('time slot table', () => {
+        const today = new Date();
+        const availableTimeSlots = [
+          { startsAt: today.setHours(9, 0, 0, 0) },
+          { startsAt: today.setHours(9, 30, 0, 0) }
+        ];
         it('renders a table for time slots', () => {
             render(<AppointmentForm />);
             expect(
@@ -135,5 +142,40 @@ describe('AppointmentForm', () => {
             expect(dates[1].textContent).toEqual('Sun 02');
             expect(dates[6].textContent).toEqual('Fri 07');
         });
+        it('renders a radio button for each time slot', () => {
+            render(
+              <AppointmentForm
+                availableTimeSlots={availableTimeSlots}
+                today={today}
+              />
+            );
+            const cells = timeSlotTable().querySelectorAll('td');
+            expect(
+              cells[0].querySelector('input[type="radio"]')
+            ).not.toBeNull();
+            expect(
+              cells[7].querySelector('input[type="radio"]')
+            ).not.toBeNull();
+          });
+      
+          it('does not render radio buttons for unavailable time slots', () => {
+            render(<AppointmentForm availableTimeSlots={[]} />);
+            const timesOfDay = timeSlotTable().querySelectorAll('input');
+            expect(timesOfDay).toHaveLength(0);
+          });
+          it('sets radio button values to the index of the corresponding appointment', () => {
+            render(
+              <AppointmentForm
+                availableTimeSlots={availableTimeSlots}
+                today={today}
+              />
+            );
+            expect(startsAtField(0).value).toEqual(
+              availableTimeSlots[0].startsAt.toString()
+            );
+            expect(startsAtField(1).value).toEqual(
+              availableTimeSlots[1].startsAt.toString()
+            );
+          });
     });
 });
