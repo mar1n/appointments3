1cfd467b66e154edd922a5af9704e4a4c5c24ea6
mar1n <30738216+mar1n@users.noreply.github.com>
2020-04-12 23:51:18 +0100

    shallow render

--- test/shallowHelpers.test.js
+++ test/shallowHelpers.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { childrenOf } from './shallowHelpers';
+import { createShallowRenderer, childrenOf, type } from './shallowHelpers';
+
 
 describe('childrenOf', () => {
     it('returns no children', () => {
@@ -31,3 +32,73 @@ describe('childrenOf', () => {
         ).toEqual([<p>A</p>]);
     });
 });
+
+const TestComponent = ({ children }) => (
+    <React.Fragment>{children}</React.Fragment>
+);
+describe('child', () => {
+    let render, child;
+
+    beforeEach(() => {
+        ({ render, child } = createShallowRenderer());
+    });
+    it('returns undefined if the child does not exist', () => {
+        render(<TestComponent />);
+        expect(child(0)).not.toBeDefined();
+    });
+    it('returns child of rendered element', () => {
+        render(
+            <TestComponent>
+                <p>A</p>
+                <p>B</p>
+            </TestComponent>
+        );
+            expect(child(1)).toEqual(<p>B</p>);
+        
+    });
+})
+
+describe('elementMatching', () => {
+    let render, elementsMatching;
+
+    beforeEach(() => {    ({ render, elementsMatching } = createShallowRenderer());  });
+    it('finds multiple direct children', () => {
+        render(
+            <TestComponent>
+                <p>A</p>
+                <p>B</p>
+            </TestComponent>
+        );
+        expect(elementsMatching(type('p'))).toEqual([
+            <p>A</p>,
+            <p>B</p>
+        ]);
+    });
+    it('finds indirect children', () => {
+        render(
+            <TestComponent>
+                <div>
+                    <p>A</p>
+                </div>
+            </TestComponent>
+        );
+        expect(elementsMatching(type('p'))).toEqual([<p>A</p>]);
+    })
+})
+
+describe('elementMatching', () => {
+    let render, elementMatching;
+
+    beforeEach(() => {
+        ({ render, elementMatching } = createShallowRenderer());
+    });
+    it('finds direct child', () => {
+        render(
+            <TestComponent>
+                <p>A</p>
+                <p>B</p>
+            </TestComponent>
+        );
+        expect(elementMatching(type('p'))).toEqual(<p>A</p>);
+    });
+})
