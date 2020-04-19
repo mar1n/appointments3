4f5991452a3c4859a155397c31c1c7f487d345fd
mar1n <30738216+mar1n@users.noreply.github.com>
2020-04-19 23:41:32 +0100

    next and previous

--- src/CustomerSearch.js
+++ src/CustomerSearch.js
@@ -1,62 +1,84 @@
 import React, { useEffect, useState, useCallback } from 'react';
 
-const CustomerRow = ({ customer }) => (
-    <tr>
-        <td>{customer.firstName}</td>
-        <td>{customer.lastName}</td>
-        <td>{customer.phoneNumber}</td>
-        <td />
-    </tr>
+const SearchButtons = ({ handleNext, handlePrevious }) => (
+  <div className="button-bar">
+    <button
+      role="button"
+      id="previous-page"
+      onClick={handlePrevious}>
+      Previous
+    </button>
+    <button role="button" id="next-page" onClick={handleNext}>
+      Next
+    </button>
+  </div>
 );
 
-const SearchButtons = ({ handleNext }) => (
-    <div className="button-bar">
-        <button role="button" id="next-page" onClick={handleNext}>
-            Next
-        </button>
-    </div>
-)
+const CustomerRow = ({ customer }) => (
+  <tr>
+    <td>{customer.firstName}</td>
+    <td>{customer.lastName}</td>
+    <td>{customer.phoneNumber}</td>
+    <td />
+  </tr>
+);
 
 export const CustomerSearch = () => {
-    const [customers, setCustomers] = useState([]);
-    const [queryString, setQueryString] = useState('');
-    useEffect(() => {
-        const fetchData = async () => {
-            const result = await window.fetch(`/customers${queryString}`, {
-                method: 'GET',
-                credentials: 'same-origin',
-                headers: { 'Content-Type': 'application/json' }
-            });
-            setCustomers(await result.json());
-        };
+  const [customers, setCustomers] = useState([]);
+  const [queryStrings, setQueryStrings] = useState([]);
 
-        fetchData();
-    }, [queryString]);
+  const handleNext = useCallback(() => {
+    const after = customers[customers.length - 1].id;
+    const queryString = `?after=${after}`;
+    setQueryStrings([...queryStrings, queryString]);
+  }, [customers, queryStrings]);
 
-    const handleNext = useCallback(async () => {
-        const after = customers[customers.length - 1].id;
-        const newQueryString = `?after=${after}`;
-        setQueryString(newQueryString);
-    }, [customers]);
+  const handlePrevious = useCallback(
+    () => setQueryStrings(queryStrings.slice(0, -1)),
+    [queryStrings]
+  );
 
-    return (
-        <>
-        <SearchButtons handleNext={handleNext} />
-        <table>
-            <thead>
-                <tr>
-                    <th>First name</th>
-                    <th>Last name</th>
-                    <th>Phone number</th>
-                    <th>Actions</th>
-                </tr>
-            </thead>
-            <tbody>
-                {customers.map(customer => (
-                    <CustomerRow customer={customer} key={customer.id} />
-                ))}
-            </tbody>
-        </table>
-        </>
-    );
+  useEffect(() => {
+    const fetchData = async () => {
+      let queryString = '';
+      if (queryStrings.length > 0)
+        queryString = queryStrings[queryStrings.length - 1];
+
+      const result = await window.fetch(
+        `/customers${queryString}`,
+        {
+          method: 'GET',
+          credentials: 'same-origin',
+          headers: { 'Content-Type': 'application/json' }
+        }
+      );
+      setCustomers(await result.json());
+    };
+
+    fetchData();
+  }, [queryStrings]);
+
+  return (
+    <React.Fragment>
+      <SearchButtons
+        handleNext={handleNext}
+        handlePrevious={handlePrevious}
+      />
+      <table>
+        <thead>
+          <tr>
+            <th>First name</th>
+            <th>Last name</th>
+            <th>Phone number</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {customers.map(customer => (
+            <CustomerRow customer={customer} key={customer.id} />
+          ))}
+        </tbody>
+      </table>
+    </React.Fragment>
+  );
 };
--- test/CustomerSearch.test.js
+++ test/CustomerSearch.test.js
@@ -5,82 +5,140 @@ import { CustomerSearch } from '../src/CustomerSearch';
 import { fetchResponseOk } from './spyHelpers';
 
 const oneCustomer = [
-    { id: 1, firstName: 'A', lastName: 'B', phoneNumber: '1' }
-  ];
+  { id: 1, firstName: 'A', lastName: 'B', phoneNumber: '1' }
+];
 
 const twoCustomers = [
-    { id: 1, firstName: 'A', lastName: 'B', phoneNumber: '1'},
-    { id: 2, firstName: 'C', lastName: 'D', phoneNumber: '2'}
+  { id: 1, firstName: 'A', lastName: 'B', phoneNumber: '1' },
+  { id: 2, firstName: 'C', lastName: 'D', phoneNumber: '2' }
 ];
 
 const tenCustomers = Array.from('0123456789', id => ({ id }));
 
-describe('CustomerSearch', () => {
-    let renderAndWait, container, element, elements, clickAndWait;
+const anotherTenCustomers = Array.from('ABCDEFGHIJ', id => ({
+  id
+}));
 
-    beforeEach(() => {
+describe('CustomerSearch', () => {
+  let renderAndWait, container, element, elements, clickAndWait;
+
+  beforeEach(() => {
     ({
-        renderAndWait,
-        container,
-        element,
-        elements,
-        clickAndWait,
+      renderAndWait,
+      container,
+      element,
+      elements,
+      clickAndWait
     } = createContainer());
     jest
-        .spyOn(window, 'fetch')
-        .mockReturnValue(fetchResponseOk([]));
-    });
+      .spyOn(window, 'fetch')
+      .mockReturnValue(fetchResponseOk([]));
+  });
 
-    it('renders a table with four headings', async () => {
-        await renderAndWait(<CustomerSearch />);
-        const heading = elements('table th');
-        expect(heading.map(h => h.textContent)).toEqual([
-            'First name', 'Last name', 'Phone number', 'Actions'
-        ]);
+  it('renders a table with four headings', async () => {
+    await renderAndWait(<CustomerSearch />);
+    const headings = elements('table th');
+    expect(headings.map(h => h.textContent)).toEqual([
+      'First name',
+      'Last name',
+      'Phone number',
+      'Actions'
+    ]);
+  });
+
+  it('fetches all customer data when component mounts', async () => {
+    await renderAndWait(<CustomerSearch />);
+    expect(window.fetch).toHaveBeenCalledWith('/customers', {
+      method: 'GET',
+      credentials: 'same-origin',
+      headers: { 'Content-Type': 'application/json' }
     });
-    it('fetches all customer data when component mounts', async () => {
-          await renderAndWait(<CustomerSearch />);
-            expect(window.fetch).toHaveBeenCalledWith('/customers', {
-                    method: 'GET',
-                    credentials: 'same-origin',
-                    headers: { 'Content-Type': 'application/json' }
-        });
-    });
-    it('renders all customer data in a table row', async () => {
-        window.fetch.mockReturnValue(fetchResponseOk(oneCustomer));
-        await renderAndWait(<CustomerSearch />);
-        const columns = elements('table > tbody > tr > td');
-        expect(columns[0].textContent).toEqual('A');
-        expect(columns[1].textContent).toEqual('B');
-        expect(columns[2].textContent).toEqual('1');
-      });
-      it('renders multiple customer rows', async () => {
-          window.fetch.mockReturnValue(fetchResponseOk(twoCustomers));
-          await renderAndWait(<CustomerSearch />);
-          const rows = elements('table tbody tr');
-          expect(rows[1].childNodes[0].textContent).toEqual('C');
-      });
-      it('has a next button', async () => {
-          await renderAndWait(<CustomerSearch />);
-          expect(element('button#next-page')).not.toBeNull();
-      });
-      it('request next page of data when next button is clicked', async () => {
-          window.fetch.mockReturnValue(fetchResponseOk(tenCustomers));
-          await renderAndWait(<CustomerSearch />);
-          await clickAndWait(element('button#next-page'));
-          expect(window.fetch).toHaveBeenLastCalledWith(
-              '/customers?after=9',
-              expect.anything()
-          );
-      });
-      it('displays next page of data when next button is clicked', async () => {
-          const nextCustomer = [{ id: 'next', firstName: 'Next' }];
-          window.fetch
-            .mockReturnValueOnce(fetchResponseOk(tenCustomers))
-            .mockReturnValue(fetchResponseOk(nextCustomer));
-        await renderAndWait(<CustomerSearch />);
-        await clickAndWait(element('button#next-page'));
-        expect(elements('tbody tr').length).toEqual(1);
-        expect(elements('td')[0].textContent).toEqual('Next');
-      });
-})
+  });
+
+  it('renders all customer data in a table row', async () => {
+    window.fetch.mockReturnValue(fetchResponseOk(oneCustomer));
+    await renderAndWait(<CustomerSearch />);
+    const columns = elements('table > tbody > tr > td');
+    expect(columns[0].textContent).toEqual('A');
+    expect(columns[1].textContent).toEqual('B');
+    expect(columns[2].textContent).toEqual('1');
+  });
+
+  it('renders multiple customer rows', async () => {
+    window.fetch.mockReturnValue(fetchResponseOk(twoCustomers));
+    await renderAndWait(<CustomerSearch />);
+    const rows = elements('table tbody tr');
+    expect(rows[1].childNodes[0].textContent).toEqual('C');
+  });
+
+  it('has a next button', async () => {
+    await renderAndWait(<CustomerSearch />);
+    expect(element('button#next-page')).not.toBeNull();
+  });
+
+  it('requests next page of data when next button is clicked', async () => {
+    window.fetch.mockReturnValue(fetchResponseOk(tenCustomers));
+    await renderAndWait(<CustomerSearch />);
+    await clickAndWait(element('button#next-page'));
+    expect(window.fetch).toHaveBeenLastCalledWith(
+      '/customers?after=9',
+      expect.anything()
+    );
+  });
+
+  it('displays next page of data when next button is clicked', async () => {
+    const nextCustomer = [{ id: 'next', firstName: 'Next' }];
+    window.fetch
+      .mockReturnValueOnce(fetchResponseOk(tenCustomers))
+      .mockReturnValue(fetchResponseOk(nextCustomer));
+    await renderAndWait(<CustomerSearch />);
+    await clickAndWait(element('button#next-page'));
+    expect(elements('tbody tr').length).toEqual(1);
+    expect(elements('td')[0].textContent).toEqual('Next');
+  });
+
+  it('has a previous button', async () => {
+    await renderAndWait(<CustomerSearch />);
+    expect(element('button#previous-page')).not.toBeNull();
+  });
+
+  it('moves back to first page when previous button is clicked', async () => {
+    window.fetch.mockReturnValue(fetchResponseOk(tenCustomers));
+    await renderAndWait(<CustomerSearch />);
+    await clickAndWait(element('button#next-page'));
+    await clickAndWait(element('button#previous-page'));
+    expect(window.fetch).toHaveBeenLastCalledWith(
+      '/customers',
+      expect.anything()
+    );
+  });
+
+  it('moves back one page when clicking previous after multiple clicks of the next button', async () => {
+    window.fetch
+      .mockReturnValueOnce(fetchResponseOk(tenCustomers))
+      .mockReturnValue(fetchResponseOk(anotherTenCustomers));
+    await renderAndWait(<CustomerSearch />);
+    await clickAndWait(element('button#next-page'));
+    await clickAndWait(element('button#next-page'));
+    await clickAndWait(element('button#previous-page'));
+    expect(window.fetch).toHaveBeenLastCalledWith(
+      '/customers?after=9',
+      expect.anything()
+    );
+  });
+
+  it('moves back multiple pages', async () => {
+    window.fetch
+      .mockReturnValueOnce(fetchResponseOk(tenCustomers))
+      .mockReturnValue(fetchResponseOk(anotherTenCustomers));
+    await renderAndWait(<CustomerSearch />);
+    await clickAndWait(element('button#next-page'));
+    await clickAndWait(element('button#next-page'));
+    await clickAndWait(element('button#previous-page'));
+    await clickAndWait(element('button#previous-page'));
+    expect(window.fetch).toHaveBeenLastCalledWith(
+      '/customers',
+      expect.anything()
+    );
+  });
+});
